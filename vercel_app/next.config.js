23796b32b49e2a7ea7c6356d720104a0f3d8039d
Sujan <sujanbhattarai.jr@gmail.com>
2025-10-26 17:38:59 -0400

    working tree clean

--- vercel_app/next.config.js
+++ vercel_app/next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
