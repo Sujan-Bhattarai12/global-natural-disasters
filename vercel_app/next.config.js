f2edfe744a69fca5200e78fd303532368c18ed46
Sujan <sujanbhattarai.jr@gmail.com>
2025-10-26 15:48:56 -0400

    add the geojason back

--- vercel_app/next.config.js
+++ vercel_app/next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
   trailingSlash: true,
+  // REMOVE: output: 'export'
 }
 
 module.exports = nextConfig
